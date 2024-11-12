8070588e7b7d0b08727a4a908c66113b613a5b95
thespielplatz <informatics@gmx.net>
2024-11-12 08:22:20 +0100

    chore: add link to thespielplatz

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center pt-20">
     <div>
-      <div class="text-2xl font-black">Free and open source tools of TheSpielplatz</div>
+      <div class="text-2xl font-black">Free and open source tools of <a class="underline" href="https://thespielplatz.com" target="_blank">TheSpielplatz</a></div>
       <div class="pt-10">
         <a class="underline" href="/wallet">LNURL Web Wallet</a>
       </div>
