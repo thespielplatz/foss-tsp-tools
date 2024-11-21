62caf7245c997fa5f9962a535a3a9e6a42bbede3
thespielplatz <informatics@gmx.net>
2024-11-21 18:49:53 +0100

    style(footer): change version to badge

--- components/layout/TheFooter.vue
+++ components/layout/TheFooter.vue
@@ -1,15 +1,15 @@
 <template>
     <CenterContainer class="border-t-4 px-6 border-unobtrusive py-4">
-      <div class="flex gap-1 justify-end">
+      <div class="flex gap-1 justify-end items-center">
         <GithubLink />
         <div>|</div>
         <Tooltip text="No cookies are used in this projects">
           <LinkDefault>Cookies</LinkDefault>
         </Tooltip>
         <div>|</div>
-        <div>
+        <Badge>
           <LinkDefault target="_blank" :href="`https://github.com/thespielplatz/foos-tsp-tools/releases/tag/${version}`">{{ version }}</LinkDefault>
-        </div>
+        </Badge>
       </div>
     </CenterContainer>
 </template>
@@ -20,15 +20,10 @@ import CenterContainer from '~/components/layout/CenterContainer.vue'
 import GithubLink from '~/components/content/GithubLink.vue'
 import Tooltip from '~/components/layout/Tooltip.vue'
 import LinkDefault from '~/components/typography/LinkDefault.vue'
+import Badge from '~/components/typography/Badge.vue'
 
 const runtimeConfig = useRuntimeConfig()
 
-const version = ref('undefined')
-
-onBeforeMount(() => {
-  if (runtimeConfig.public.version) {
-    version.value = `v${runtimeConfig.public.version}`
-  }
-})
+const version = ref(runtimeConfig.public.version ? `v${runtimeConfig.public.version}` : 'undefined')
 
 </script>
